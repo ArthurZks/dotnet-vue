868e893e1a26eb3ba8eef69a35d3bb2c0299c8f2
zks <kesheng.zeng@binqsoft.com>
2019-12-06 17:48:54 +0800

    first commit

---
+++ ClientApp/components/project/project.ts
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import {Component} from "vue-property-decorator";
+const echarts = require('echarts');
+
+interface Project {
+    title: string;
+    name: string;
+    
+}
+
+// @Component
+// export default class ProjectComponent exte
